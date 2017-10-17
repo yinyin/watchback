f9bb0ca66b3852a040ad0ce8cc659519374ccddc
Yinyin L <yinyinl@gmail.com>
2017-10-17 19:05:35 +0800

    implement availability check logic
    * available period must explicitly given
    * blackout period will override available period

---
+++ availability.go
@@ -0,0 +1,55 @@
+package watchback
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+const maxAvailabilityDataAtomicOperationAttempt = 8
+
+func extendTimeBoundary(d time.Duration, p * int64) {
+	t := time.Now().Add(d).UnixNano()
+	for attempt := 0; attempt < maxAvailabilityDataAtomicOperationAttempt; attempt++ {
+		prevT := atomic.LoadInt64(p)
+		if t > prevT {
+			if atomic.CompareAndSwapInt64(p, prevT, t) {
+				return
+			}
+		}
+	}
+}
+
+type availabilityLogic struct {
+	availableUntil int64
+	blackoutUntil int64
+}
+
+func newAvailabilityLogic() (l availabilityLogic) {
+	return availabilityLogic{
+		availableUntil: 0,
+		blackoutUntil: 0,
+	}
+}
+
+func (a * availabilityLogic) Reset() {
+	atomic.StoreInt64(&a.availableUntil, 0)
+	atomic.StoreInt64(&a.blackoutUntil, 0)
+}
+
+func (a * availabilityLogic) AvailableWithin(d time.Duration) {
+	extendTimeBoundary(d, &a.availableUntil)
+}
+
+func (a * availabilityLogic) BlackoutWithin(d time.Duration) {
+	extendTimeBoundary(d, &a.blackoutUntil)
+}
+
+func (a * availabilityLogic) Availability() (available bool) {
+	c := time.Now().UnixNano()
+	availableUtil := atomic.LoadInt64(&a.availableUntil)
+	blackoutUtil := atomic.LoadInt64(&a.blackoutUntil)
+	if (c > blackoutUtil) && (c <= availableUtil) {
+		return true
+	}
+	return false
+}
---
+++ availability_test.go
@@ -0,0 +1,74 @@
+package watchback
+
+import (
+	"testing"
+	"time"
+)
+
+func TestAvailabilityLogic_Availability_available1(t *testing.T) {
+	l := newAvailabilityLogic()
+	l.AvailableWithin(time.Second * 1)
+	if !l.Availability() {
+		t.Errorf("expect logic available.")
+	}
+	time.Sleep(time.Second * 2)
+	if l.Availability() {
+		t.Errorf("expect logic not available.")
+	}
+}
+
+func TestAvailabilityLogic_Availability_blackout1(t *testing.T) {
+	l := newAvailabilityLogic()
+	l.AvailableWithin(time.Second * 3)
+	if !l.Availability() {
+		t.Errorf("expect logic available.")
+	}
+	l.BlackoutWithin(time.Second * 1)
+	if l.Availability() {
+		t.Errorf("expect logic blackouted.")
+	}
+	time.Sleep(time.Second * 2)
+	if !l.Availability() {
+		t.Errorf("expect logic available after blackout.")
+	}
+	time.Sleep(time.Second * 2)
+	if l.Availability() {
+		t.Errorf("expect logic not available.")
+	}
+}
+
+func TestAvailabilityLogic_Availability_blackout2(t *testing.T) {
+	l := newAvailabilityLogic()
+	l.AvailableWithin(time.Second * 3)
+	l.BlackoutWithin(time.Second * 1)
+	l.AvailableWithin(time.Second * 1)
+	if l.Availability() {
+		t.Errorf("expect logic blackouted even with available-within followed.")
+	}
+	time.Sleep(time.Second * 2)
+	if !l.Availability() {
+		t.Errorf("expect logic available after blackout.")
+	}
+}
+
+func TestAvailabilityLogic_Reset(t *testing.T) {
+	l := newAvailabilityLogic()
+	if l.Availability() {
+		t.Errorf("expect logic not available (new).")
+	}
+	l.AvailableWithin(time.Second * 3)
+	if !l.Availability() {
+		t.Errorf("expect logic available (available-within).")
+	}
+	l.Reset()
+	if l.Availability() {
+		t.Errorf("expect logic not available (reset).")
+	}
+	l.AvailableWithin(time.Second * 3)
+	l.BlackoutWithin(time.Second * 3)
+	l.Reset()
+	l.AvailableWithin(time.Second * 1)
+	if !l.Availability() {
+		t.Errorf("expect logic available (reset, available-within).")
+	}
+}
