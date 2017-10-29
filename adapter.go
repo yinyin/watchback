955dc660272d5f4a8e62472b5165f251d63b9a98
Yinyin L <yinyinl@gmail.com>
2017-10-29 17:20:47 +0800

    change prepare and off-service check interface

--- adapter.go
+++ adapter.go
@@ -3,10 +3,11 @@ package watchback
 import "context"
 
 type NodeMessagingAdapter interface {
-	// Return true if fetched status is in Normal state.
-	// Return false for protocol which status can be determine within FetchStatus() function.
-	// This function might be invoke in parallel with FetchStatus().
-	CheckStatusNormal(ctx context.Context) (isNormal bool, err error)
+	// Notice adapter there is an error on messaging operation
+	HasMessagingFailure(err error)
+
+	// Check if remote node is running service.
+	IsOnService(ctx context.Context) (onService bool, err error)
 
 	// Emit service activation announcement to remote node.
 	// Return true if service activation approval request is approved.
@@ -14,13 +15,25 @@ type NodeMessagingAdapter interface {
 
 	// Close node connection link
 	Close(ctx context.Context) (err error)
+
+
+	// Return true if fetched status is in Normal state.
+	// Return false for protocol which status can be determine within FetchStatus() function.
+	// This function might be invoke in parallel with FetchStatus().
+	// -- CheckStatusNormal(ctx context.Context) (isNormal bool, err error)
 }
 
 // Represent a local instance of service.
 type ServiceControlAdapter interface {
 	// Perform initialize. Service rack status is not ready yet when calling this function.
 	// Return value will be logged and treated as self-check result.
-	Prepare() (err error)
+	Prepare(ctx context.Context) (err error)
+
+	// Perform self-check to see if local environment is good for starting service.
+	// Return nil if passed self-check.
+	// Return error if failed on self-check.
+	OffServiceSelfCheck(ctx context.Context) (err error)
+
 
 	// Perform initialize. Service rack status is ready before calling this function.
 	// Return value will be logged and treated as self-check result.
